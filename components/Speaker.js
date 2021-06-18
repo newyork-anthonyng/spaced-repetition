8eb19c40793557b9f5f8bfd3754207c3e60ab772
Anthony Ng <newyork.anthonyng@gmail.com>
2021-06-18 19:50:39 -0400

    stop Speaker from playing concurrent sounds

--- components/Speaker.js
+++ components/Speaker.js
@@ -2,10 +2,44 @@ import React from "react";
 import { View, Image, Pressable, StyleSheet } from "react-native";
 import { Audio } from "expo-av";
 import speaker from "../assets/speaker.png";
+import { createMachine } from "xstate";
+import { useMachine } from "@xstate/react";
+
+const machine = createMachine({
+  id: "speaker",
+  initial: "idle",
+  states: {
+    idle: {
+      on: {
+        play: "playing",
+      },
+    },
+    playing: {
+      entry: "playSound",
+      on: {
+        done: "idle",
+      },
+    },
+  },
+});
 
 function Speaker({ src, onPlay }) {
+  const [state, send] = useMachine(machine, {
+    actions: {
+      playSound,
+    },
+  });
+
   async function playSound() {
-    const { sound } = await Audio.Sound.createAsync({ uri: src });
+    const { sound } = await Audio.Sound.createAsync(
+      { uri: src },
+      {},
+      (status) => {
+        if (status.didJustFinish) {
+          send("done");
+        }
+      }
+    );
 
     await sound.playAsync();
     if (onPlay) {
@@ -13,9 +47,13 @@ function Speaker({ src, onPlay }) {
     }
   }
 
+  function handlePress() {
+    send("play");
+  }
+
   return (
     <View style={styles.container}>
-      <Pressable onPress={playSound}>
+      <Pressable onPress={handlePress} style={styles.pressable}>
         <Image source={speaker} style={styles.image} />
       </Pressable>
     </View>
@@ -31,9 +69,11 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 5,
     backgroundColor: "white",
-    padding: 30,
     borderRadius: 25,
     width: 175,
+  },
+  pressable: {
+    padding: 30,
   },
   image: {
     height: 100,
